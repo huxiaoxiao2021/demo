dc0f98734ba5adf29e3e7e6dfd2f6e430b693020
hujiping1 <hujiping@jd.com>
2024-05-11 18:37:42 +0800

    demo init

---
+++ src/main/java/com/example/demo/learn/Ready.java
@@ -0,0 +1,38 @@
+package com.example.demo.learn;
+
+/**
+ * 类的描述
+ *
+ * @author hujiping
+ * @date 2024/5/11 3:47 PM
+ */
+public class Ready {
+
+    public static void main(String[] args) {
+        
+        // 开发软件：
+            // idea：https://www.jetbrains.com/zh-cn/idea/download/other.html
+            // idea激活方案：https://r86oxhhvu2.feishu.cn/docs/doccnJQLb5Fv4cSOx0uDJdcuWdh
+            // git：https://git-scm.com/download/win
+            // maven：https://maven.apache.org/download.cgi
+            // jdk21：https://adoptium.net/zh-CN/temurin/releases/?os=windows&package=jdk&arch=x64&version=21
+            // mysql Navicat：https://www.navicat.com/en/download/navicat-for-mysql#windows
+            // VScode：https://code.visualstudio.com/download
+            // apiPost：https://apifox.com/?utm_source=google_search&utm_campaign=15676663585&utm_content=131096021026&utm_term=apipost&gad_source=1&gclid=CjwKCAjwrvyxBhAbEiwAEg_KghJ0uVFGZEEsl-CUApqCYueyN05umUX0mmMAgSYZzleOnWlW0RAKKhoCCM4QAvD_BwE
+            // mat：https://eclipse.dev/mat/downloads.php
+            // SwitchHosts：https://github.com/oldj/SwitchHosts/releases
+            // tomcat：https://tomcat.apache.org/download-80.cgi
+        // springBoot基础项目提前准备好，下载github项目改名后转换成自己的并上传到自己的git仓库
+            // git clone <original_repository_URL> <new_project_name>
+            // cd <new_project_name>
+            // git init
+            // git remote add origin <your_new_repository_URL>
+            // git add .
+            // git commit -m "Initial commit"
+            // git push -u origin master
+        // jvm排查工具mat使用
+        // 上线打包发布
+        // springCloud全家桶使用、dubbo、rocketMq、sential等中间件使用
+        // chatGpt注册使用
+    }
+}
